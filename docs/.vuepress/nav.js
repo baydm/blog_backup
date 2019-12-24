46d455e2050d9526fa6041064d007edd1305950c
baydm <wywaigua@163.com>
2019-12-24 11:05:14 +0800

    备份博客源码

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -4,5 +4,6 @@ module.exports = [
       { text: '随笔', link: '/essay/' },
       { text: '算法', link: '/algorithm/'},
       { text: '知识库', link: 'https://www.yuque.com/mdev' },
+      { text: '漫画', link: '/manhua/' },
       { text: '关于我', link: '/about/' }
 ]
