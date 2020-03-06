f0f7577c1100ae138fcad5fb5fcb72afc976a2e2
baydm <wywaigua@163.com>
2020-03-06 18:41:50 +0800

    备份博客源码

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -4,6 +4,6 @@ module.exports = [
       { text: '随笔', link: '/essay/' },
       { text: '算法', link: '/algorithm/'},
       { text: '知识库', link: 'https://www.yuque.com/mdev' },
-      { text: '漫画', link: '/manhua/index.html' },
+      { text: '原型图', link: '/cs/index.html' },
       { text: '关于我', link: '/about/' }
 ]
